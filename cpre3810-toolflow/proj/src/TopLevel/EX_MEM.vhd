732878508a7b6022898a8c4a4f28fdfcbe9aa7b7
teoh <teoh@linuxvdi-f25-46.ece.iastate.edu>
2025-11-11 13:04:33 -0600

    EX_MEM adjusted

--- cpre3810-toolflow/proj/src/TopLevel/EX_MEM.vhd
+++ cpre3810-toolflow/proj/src/TopLevel/EX_MEM.vhd
@@ -2,6 +2,7 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity EX_MEM is
+generic ( N : integer := 32 );
   port(
     -- Clock / Reset / Write-enable (stall)
     i_CLK : in  std_logic;
@@ -10,12 +11,14 @@ entity EX_MEM is
 
     -- Inputs from EX stage 
     i_ALUResult   : in  std_logic_vector(31 downto 0);
-    i_PCbrOrJmp   : in  std_logic_vector(31 downto 0);
-    i_Zero        : in  std_logic;
-    i_LT          : in  std_logic;
-    i_LTU         : in  std_logic;
+    --i_PCbrOrJmp   : in  std_logic_vector(31 downto 0);
+    --i_Zero        : in  std_logic;
+    --i_LT          : in  std_logic;
+    --i_LTU         : in  std_logic;
     i_RS2         : in  std_logic_vector(31 downto 0);
     i_rd          : in  std_logic_vector(4 downto 0);
+    i_PCplus4   : in  std_logic_vector(N-1 downto 0);
+    i_Imm       : in  std_logic_vector(31 downto 0);
 
     -- Control to carry forward
     i_MemWrite    : in  std_logic;
@@ -23,26 +26,21 @@ entity EX_MEM is
     i_ResultSrc   : in  std_logic_vector(1 downto 0);
     i_RegWrite    : in  std_logic;
     i_LoadType    : in  std_logic_vector(2 downto 0);
-    i_Jump        : in  std_logic;
-    i_Branch      : in  std_logic;
+    --i_Jump        : in  std_logic;
+    --i_Branch      : in  std_logic;
     i_Halt        : in  std_logic;
 
     -- Latched outputs to MEM stage
-    exmem_ALUResult : out std_logic_vector(31 downto 0);
-    exmem_PCbrOrJmp : out std_logic_vector(31 downto 0);
-    exmem_Zero      : out std_logic;
-    exmem_LT        : out std_logic;
-    exmem_LTU       : out std_logic;
-    exmem_RS2       : out std_logic_vector(31 downto 0);
+    exmem_ALU       : out std_logic_vector(31 downto 0);
+    exmem_oRS2      : out std_logic_vector(31 downto 0);
     exmem_rd        : out std_logic_vector(4 downto 0);
-
+    exmem_PCplus4   : out std_logic_vector(N-1 downto 0);
+    exmem_Imm       : out std_logic_vector(31 downto 0);
     exmem_MemWrite  : out std_logic;
     exmem_MemRead   : out std_logic;
-    exmem_ResultSrc : out std_logic_vector(1 downto 0);
     exmem_RegWrite  : out std_logic;
+    exmem_ResultSrc : out std_logic_vector(1 downto 0);
     exmem_LoadType  : out std_logic_vector(2 downto 0);
-    exmem_Jump      : out std_logic;
-    exmem_Branch    : out std_logic;
     exmem_Halt      : out std_logic
   );
 end EX_MEM;
@@ -72,26 +70,22 @@ architecture structural of EX_MEM is
   end component;
 begin
   -- 32-bit datapath registers
-  r_ALUResult : RegN
-    generic map(N => 32)
-    port map(i_CLK => i_CLK, i_RST => i_RST, i_WE => i_WE,
-             i_D => i_ALUResult,   o_Q => exmem_ALUResult);
+   r_ALU    : RegN generic map(N=>32) port map(i_CLK=>i_CLK,i_RST=>i_RST,i_WE=>i_WE, i_D=>i_ALUResult,     o_Q=>exmem_ALU);
 
-  r_PCbrOrJmp : RegN
-    generic map(N => 32)
-    port map(i_CLK => i_CLK, i_RST => i_RST, i_WE => i_WE,
-             i_D => i_PCbrOrJmp,   o_Q => exmem_PCbrOrJmp);
+  r_PC4    : RegN generic map(N=>N ) port map(i_CLK=>i_CLK,i_RST=>i_RST,i_WE=>i_WE, i_D=>i_PCplus4, o_Q=>exmem_PCplus4);
 
   r_RS2 : RegN
     generic map(N => 32)
     port map(i_CLK => i_CLK, i_RST => i_RST, i_WE => i_WE,
-             i_D => i_RS2,         o_Q => exmem_RS2);
+             i_D => i_RS2,         o_Q => exmem_oRS2);
 
   -- 5-bit destination register (rd)
   r_rd : RegN
     generic map(N => 5)
     port map(i_CLK => i_CLK, i_RST => i_RST, i_WE => i_WE,
              i_D => i_rd,          o_Q => exmem_rd);
+
+  r_Imm    : RegN generic map(N=>32) port map(i_CLK=>i_CLK,i_RST=>i_RST,i_WE=>i_WE, i_D=>i_Imm,     o_Q=>exmem_Imm);
 
   -- Multi-bit control bundles
   r_ResultSrc : RegN
@@ -104,16 +98,12 @@ begin
     port map(i_CLK => i_CLK, i_RST => i_RST, i_WE => i_WE,
              i_D => i_LoadType,     o_Q => exmem_LoadType);
 
-  -- 1-bit flags / controls
-  r_Zero     : dffg port map(i_CLK => i_CLK, i_RST => i_RST, i_WE => i_WE, i_D => i_Zero,     o_Q => exmem_Zero);
-  r_LT       : dffg port map(i_CLK => i_CLK, i_RST => i_RST, i_WE => i_WE, i_D => i_LT,       o_Q => exmem_LT);
-  r_LTU      : dffg port map(i_CLK => i_CLK, i_RST => i_RST, i_WE => i_WE, i_D => i_LTU,      o_Q => exmem_LTU);
-
+  -- Single-bit control signals
   r_MemWrite : dffg port map(i_CLK => i_CLK, i_RST => i_RST, i_WE => i_WE, i_D => i_MemWrite, o_Q => exmem_MemWrite);
   r_MemRead  : dffg port map(i_CLK => i_CLK, i_RST => i_RST, i_WE => i_WE, i_D => i_MemRead,  o_Q => exmem_MemRead);
   r_RegWrite : dffg port map(i_CLK => i_CLK, i_RST => i_RST, i_WE => i_WE, i_D => i_RegWrite, o_Q => exmem_RegWrite);
-  r_Jump     : dffg port map(i_CLK => i_CLK, i_RST => i_RST, i_WE => i_WE, i_D => i_Jump,     o_Q => exmem_Jump);
-  r_Branch   : dffg port map(i_CLK => i_CLK, i_RST => i_RST, i_WE => i_WE, i_D => i_Branch,   o_Q => exmem_Branch);
+  -- r_Jump     : dffg port map(i_CLK => i_CLK, i_RST => i_RST, i_WE => i_WE, i_D => i_Jump,     o_Q => exmem_Jump);
+  -- r_Branch   : dffg port map(i_CLK => i_CLK, i_RST => i_RST, i_WE => i_WE, i_D => i_Branch,   o_Q => exmem_Branch);
   r_Halt     : dffg port map(i_CLK => i_CLK, i_RST => i_RST, i_WE => i_WE, i_D => i_Halt,     o_Q => exmem_Halt);
 
 end structural;
